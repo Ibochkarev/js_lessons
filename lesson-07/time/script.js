93d099079baf2ec5713604341ec94f320e5374d7
Bochkarev Ivan <ivanx86@gmail.com>
2018-11-30 15:13:42 +0600

    Add lessons 07

---
+++ lesson-07/time/script.js
@@ -0,0 +1,20 @@
+window.addEventListener('DOMContentLoaded', function () {
+  function myClock() {
+    let siteTime = new Date();
+    let hour = siteTime.getHours();
+    let minute = siteTime.getMinutes();
+    let second = siteTime.getSeconds();
+    if (hour < 10) {
+      hour = "0" + hour;
+    }
+    if (minute < 10) {
+      minute = "0" + minute;
+    }
+    if (second < 10) {
+      second = "0" + second;
+    }
+    document.getElementById("time").innerHTML = hour + ":" + minute + ":" + second;
+  }
+
+  setInterval(myClock, 1000);
+});
