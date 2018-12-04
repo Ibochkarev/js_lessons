1efdef09cb95c0b698d0462aae56ead9f12f491b
Bochkarev Ivan <ivanx86@gmail.com>
2018-12-04 22:28:52 +0600

    Remove trush

--- lessons-10/class/script.js
+++ lessons-10/class/script.js
@@ -15,11 +15,8 @@ class Options {
     ${this.width}px; background: ${this.bg}; font-size: 
     ${this.fontSize}px; text-align: ${this.textAlign}`;
     div.textContent = text;
-    elem.appendChild(div);
+    document.body.appendChild(div);
   }
 }
-
-let elem = document.body;
-elem.style.cssText = 'color: red; font-size: 20px; font-weight: bold;';
 
 let div = new Options(700, 1200, '#ccc', 16, 'center').createNewDiv('тут должен быть текст');
