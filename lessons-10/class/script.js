726230edb1d3162025047b02df58a31e485bcf60
Bochkarev Ivan <ivanx86@gmail.com>
2018-12-04 22:07:39 +0600

    Add elem.style.cssText

--- lessons-10/class/script.js
+++ lessons-10/class/script.js
@@ -1,27 +1,34 @@
-  'use strict';
+'use strict';
 
-  class Options {
-    constructor(height = 'auto', width = '100%', bg = '#ccc', fontSize = '16px', textAlign = 'center') {
-      this.height = height;
-      this.width = width;
-      this.bg = bg;
-      this.fontSize = fontSize;
-      this.textAlign = textAlign;
-    }
-
-    createNewDiv(elem, text) {
-      let div = document.createElement('div');
-      div.style.height = this.height;
-      div.style.height = this.height;
-      div.style.width = this.width;
-      div.style.background = this.bg;
-      div.style.fontSize = this.fontSize;
-      div.style.textAlign = this.textAlign;
-      div.textContent = text;
-      elem.appendChild(div);
-    }
+class Options {
+  constructor(
+    height = 'auto',
+    width = '700px',
+    bg = '#ccc',
+    fontSize = '16px',
+    textAlign = 'center'
+  ) {
+    this.height = height;
+    this.width = width;
+    this.bg = bg;
+    this.fontSize = fontSize;
+    this.textAlign = textAlign;
   }
 
-  let elem = document.body;
+  createNewDiv(elem, text) {
+    let div = document.createElement('div');
+    div.style.height = this.height;
+    div.style.height = this.height;
+    div.style.width = this.width;
+    div.style.background = this.bg;
+    div.style.fontSize = this.fontSize;
+    div.style.textAlign = this.textAlign;
+    div.textContent = text;
+    elem.appendChild(div);
+  }
+}
 
-  let div = new Options('100%', '700px', 'aqua', '18px', 'left').createNewDiv(elem, 'тут должен быть текст');
+let elem = document.body;
+elem.style.cssText = 'color: red; font-size: 20px; font-weight: bold;';
+
+let div = new Options().createNewDiv(elem, 'тут должен быть текст');
