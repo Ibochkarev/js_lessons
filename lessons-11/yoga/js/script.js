1c4788e566d46173f8d8306a0b560763b3dd38f3
Bochkarev Ivan <ivanx86@gmail.com>
2018-12-05 23:59:24 +0600

    Fix errors

--- lessons-11/yoga/js/script.js
+++ lessons-11/yoga/js/script.js
@@ -1,11 +1,11 @@
-window.addEventListener('DOMContentLoaded', function () {
+window.addEventListener('DOMContentLoaded', function() {
   'use strict';
 
   let tab = document.querySelectorAll('.info-header-tab'),
     info = document.querySelector('.info-header'),
     tabContent = document.querySelectorAll('.info-tabcontent');
 
-  const hideTabContent = (a) => {
+  const hideTabContent = a => {
     for (let i = a; i < tabContent.length; i++) {
       tabContent[i].classList.remove('show');
       tabContent[i].classList.add('hide');
@@ -14,14 +14,14 @@ window.addEventListener('DOMContentLoaded', function () {
 
   hideTabContent(1);
 
-  const showTabContent = (b) => {
+  const showTabContent = b => {
     if (tabContent[b].classList.contains('hide')) {
       tabContent[b].classList.remove('hide');
       tabContent[b].classList.add('show');
     }
   };
 
-  info.addEventListener('click', function (event) {
+  info.addEventListener('click', function(event) {
     let target = event.target;
     if (target && target.classList.contains('info-header-tab')) {
       for (let i = 0; i < tab.length; i++) {
@@ -37,27 +37,27 @@ window.addEventListener('DOMContentLoaded', function () {
   // Timer
   let deadline = '2018-12-5';
 
-  const getTimeRemaining = (endtime) => {
+  const getTimeRemaining = endtime => {
     let t = Date.parse(endtime) - Date.parse(new Date()),
       seconds = Math.floor((t / 1000) % 60),
       minutes = Math.floor((t / 1000 / 60) % 60),
       hours = Math.floor(t / (1000 * 60 * 60));
 
     if (seconds < 10) {
-      seconds = "0" + seconds;
+      seconds = '0' + seconds;
     }
     if (minutes < 10) {
-      minutes = "0" + minutes;
+      minutes = '0' + minutes;
     }
     if (hours < 10) {
-      hours = "0" + hours;
+      hours = '0' + hours;
     }
 
     return {
-      'total': t,
-      'hours': hours,
-      'minutes': minutes,
-      'seconds': seconds
+      total: t,
+      hours: hours,
+      minutes: minutes,
+      seconds: seconds
     };
   };
 
@@ -75,9 +75,9 @@ window.addEventListener('DOMContentLoaded', function () {
 
       if (t.total <= 0) {
         clearInterval(timeInterval);
-        hours.textContent = "00";
-        minutes.textContent = "00";
-        seconds.textContent = "00";
+        hours.textContent = '00';
+        minutes.textContent = '00';
+        seconds.textContent = '00';
       }
     };
     let timeInterval = setInterval(updateClock, 1000);
@@ -91,13 +91,13 @@ window.addEventListener('DOMContentLoaded', function () {
     overlay = document.querySelector('.overlay'),
     close = document.querySelector('.popup-close');
 
-  more.addEventListener('click', function () {
+  more.addEventListener('click', function() {
     overlay.style.display = 'block';
     this.classList.add('none');
     document.body.style.overflow = 'hidden';
   });
 
-  close.addEventListener('click', function () {
+  close.addEventListener('click', function() {
     overlay.style.display = 'none';
     more.classList.remove('more-splash');
     document.body.style.overflow = '';
@@ -107,7 +107,7 @@ window.addEventListener('DOMContentLoaded', function () {
 
   let infoWrapper = document.querySelector('.info');
 
-  infoWrapper.addEventListener('click', function (event) {
+  infoWrapper.addEventListener('click', function(event) {
     let target = event.target;
     if (target && target.classList.contains('description-btn')) {
       overlay.style.display = 'block';
@@ -119,122 +119,83 @@ window.addEventListener('DOMContentLoaded', function () {
   //Forms
 
   let message = {
-    loading: "Загрузка...",
-    success: "Спасибо! Скоро мы с вами свяжемся!",
-    failure: "Что-то пошло не так..."
+    loading: 'Загрузка...',
+    success: 'Спасибо! Скоро мы с вами свяжемся!',
+    failure: 'Что-то пошло не так...'
   };
 
   let form = document.querySelector('.main-form'),
     input = form.getElementsByTagName('input'),
     statusMessage = document.createElement('div'),
-    form2 = document.querySelector('#form'),
-    input2 = form2.getElementsByTagName('input'),
-    inputPhone = document.getElementsByName('phone');
+    form2 = document.querySelector('form'),
+    inputsGroups = document.querySelectorAll('input');
+
+  inputsGroups.forEach(function(item) {
+    if (item.type == 'tel') {
+      item.addEventListener('keydown', function(e) {
+        if (!/\d|\+/gm.test(e.key) && e.keyCode != 8) {
+          e.preventDefault();
+        }
+        if (item.value.indexOf('+') != -1 && e.key == '+') {
+          e.preventDefault();
+        }
+      });
+    }
+  });
+
   statusMessage.classList.add('status');
 
-  console.log(inputPhone);
+  [form, form2].forEach(function(item) {
+    item.addEventListener('submit', function(event) {
+      event.preventDefault();
+      item.appendChild(statusMessage);
 
-  form.addEventListener('submit', function (event) {
-    event.preventDefault();
-    form.appendChild(statusMessage);
+      let request = new XMLHttpRequest();
+      request.open('POST', 'server.php');
+      request.setRequestHeader(
+        'Content-type',
+        'application/json; charset=utf-8'
+      );
 
-    let request = new XMLHttpRequest();
-    request.open('POST', 'server.php');
-    request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+      let formData = new FormData(item);
 
-    let formData = new FormData(form);
+      let obj = {};
 
-    let obj = {};
+      formData.forEach(function(value, key) {
+        obj[key] = value;
+      });
+      let json = JSON.stringify(obj);
 
-    formData.forEach(function (value, key) {
-      obj[key] = value;
-    });
-    let json = JSON.stringify(obj);
+      request.send(json);
 
-    request.send(json);
+      request.addEventListener('readystatechange', function() {
+        if (request.readyState < 4) {
+          //statusMessage.innerHTML = message.loading;
+          statusMessage.innerHTML = '';
+          let img = document.createElement('img');
+          img.src = 'img/ajax-loader.gif';
+          statusMessage.appendChild(img);
+        } else if (request.readyState == 4 && request.status == 200) {
+          statusMessage.innerHTML = '';
+          let success = document.createElement('img');
+          success.src = 'img/checked.png';
+          success.width = 32;
+          success.height = 32;
+          statusMessage.appendChild(success);
+        } else {
+          //statusMessage.innerHTML = message.failure;
+          statusMessage.innerHTML = '';
+          let failure = document.createElement('img');
+          failure.src = 'img/cancel.png';
+          failure.width = 32;
+          failure.height = 32;
+          statusMessage.appendChild(failure);
+        }
+      });
 
-    request.addEventListener('readystatechange', function () {
-      if (request.readyState < 4) {
-        //statusMessage.innerHTML = message.loading;
-        statusMessage.innerHTML = "";
-        let img = document.createElement("img");
-        img.src = "img/ajax-loader.gif";
-        statusMessage.appendChild(img);
-      } else if (request.readyState == 4 && request.status == 200) {
-        statusMessage.innerHTML = "";
-        let success = document.createElement("img");
-        success.src = "img/checked.png";
-        success.width = 32;
-        success.height = 32;
-        statusMessage.appendChild(success);
-      } else {
-        //statusMessage.innerHTML = message.failure;
-        statusMessage.innerHTML = "";
-        let alarm = document.createElement("img");
-        alarm.src = "img/cancel.png";
-        alarm.width = 32;
-        alarm.height = 32;
-        statusMessage.appendChild(alarm);
+      for (let i = 0; i < input.length; i++) {
+        input[i].value = '';
       }
     });
-
-    for (let i = 0; i < input.length; i++) {
-      input[i].value = '';
-    }
   });
-
-  form2.addEventListener('submit', function (event) {
-    event.preventDefault();
-    form2.appendChild(statusMessage);
-
-    let request = new XMLHttpRequest();
-    request.open('POST', 'server.php');
-    request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
-
-    let formData = new FormData(form2);
-
-    let obj = {};
-
-    formData.forEach(function (value, key) {
-      obj[key] = value;
-    });
-    let json = JSON.stringify(obj);
-
-    request.send(json);
-
-    request.addEventListener('readystatechange', function () {
-      if (request.readyState < 4) {
-        //statusMessage.innerHTML = message.loading;
-        statusMessage.innerHTML = "";
-        let img = document.createElement("img");
-        img.src = "img/ajax-loader.gif";
-        statusMessage.appendChild(img);
-      } else if (request.readyState == 4 && request.status == 200) {
-        statusMessage.innerHTML = "";
-        let success = document.createElement("img");
-        success.src = "img/checked.png";
-        success.width = 32;
-        success.height = 32;
-        statusMessage.appendChild(success);
-      } else {
-        //statusMessage.innerHTML = message.failure;
-        statusMessage.innerHTML = "";
-        let alarm = document.createElement("img");
-        alarm.src = "img/cancel.png";
-        alarm.width = 32;
-        alarm.height = 32;
-        statusMessage.appendChild(alarm);
-      }
-    });
-
-    for (let i = 0; i < input2.length; i++) {
-      input2[i].value = '';
-    }
-  });
-
-  for (let i = 0; i < inputPhone.length; i++) {
-    inputPhone[i].addEventListener('input', () => {
-      inputPhone[i].value = inputPhone[i].value.replace(/[^\+?\d]/g, '');
-    });
-  }
 });
